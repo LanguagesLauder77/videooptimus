cbc92245ebb28c4ff1e776c32617b5684ac09b30
LanguagesLauder77 <115863135+LanguagesLauder77@users.noreply.github.com>
2023-11-15 07:12:37 +0530

    add loading gif

--- src/Combine.js
+++ src/Combine.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import AWS from 'aws-sdk';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css'; // Ensure your custom CSS styles are correctly set up
-import loginImage from './logo4.png'; // Verify the image path is correct
+import './styles.css';
+import loginImage from './logo4.png';
 import { NavLink } from 'react-router-dom';
+import loadingGif from './loading.gif'; // Make sure this path is correct
 
-// AWS SDK configuration
 AWS.config.update({
     accessKeyId: 'AKIAVLCBUVXLEGIUGHX4',
     secretAccessKey: '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',
@@ -15,26 +15,24 @@ AWS.config.update({
 function MergedComponent() {
     const [asin, setAsin] = useState('');
     const [result, setResult] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleChangeAsin = (event) => {
         setAsin(event.target.value);
     };
 
     const invokeLambdaFunction = async () => {
+        setIsLoading(true);
         const lambda = new AWS.Lambda();
         const params = {
-            FunctionName: 'creativeAttributes', // Replace with your Lambda function name
+            FunctionName: 'creativeAttributes',
             Payload: JSON.stringify({ asin: asin })
         };
     
         try {
             const lambdaResult = await lambda.invoke(params).promise();
-            console.log('Lambda result:', lambdaResult); // Log the raw lambda result
-    
-            // First parse the Payload from the Lambda function
             const parsedLambdaResult = JSON.parse(lambdaResult.Payload);
     
-            // Now parse the 'body' within the Payload
             if (typeof parsedLambdaResult.body === 'string') {
                 parsedLambdaResult.body = JSON.parse(parsedLambdaResult.body);
             }
@@ -43,66 +41,61 @@ function MergedComponent() {
         } catch (error) {
             console.error('Error invoking Lambda function:', error);
             setResult({ error: 'Error invoking Lambda function' });
+        } finally {
+            setIsLoading(false);
         }
     };
-    
-    
-    
 
-    // Render the result in table format
     const renderResult = () => {
         if (result && result.body) {
-          const bodyData = typeof result.body === 'string' ? JSON.parse(result.body) : result.body;
-          const dynamodbCsvData = bodyData.dynamodb_csv_data;
-          const rankingData = bodyData.ranking_data;
+            const bodyData = typeof result.body === 'string' ? JSON.parse(result.body) : result.body;
+            const dynamodbCsvData = bodyData.dynamodb_csv_data;
+            const rankingData = bodyData.ranking_data;
       
-          return (
-            <>
-              {/* Render DynamoDB and CSV Data */}
-              <h3>Suggested key features to capture</h3>
-              <table className="table table-striped mt-3 custom-table">
-                <thead>
-                  <tr><th>Product features</th></tr>
-                </thead>
-                <tbody>
-                  {dynamodbCsvData.csv_data.map((item, index) => (
-                    <tr key={index}>
-                      <td>{item['Attribute Name']}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+            return (
+                <>
+                    <h3>Suggested key features to capture</h3>
+                    <table className="table table-striped mt-3 custom-table">
+                        <thead>
+                            <tr><th>Product features</th></tr>
+                        </thead>
+                        <tbody>
+                            {dynamodbCsvData.csv_data.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item['Attribute Name']}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
       
-              {/* Render Ranking Data */}
-              <h3>Product Category Best Sellers</h3>
-              <table className="table table-striped mt-3 custom-table">
-                <thead>
-                  <tr>
-                    <th>ASIN</th>
-                    <th>Link</th>
-                    <th>Image</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {rankingData.map((item, index) => (
-                    <tr key={index}>
-                      <td>{item.asin}</td>
-                      <td>
-                        <a href={item.link} target="_blank" rel="noopener noreferrer">Product Link</a>
-                      </td>
-                      <td>
-                        <img src={item.image} alt={`Product ${index}`} style={{ width: "100px", height: "auto" }} />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </>
-          );
+                    <h3>Product Category Best Sellers</h3>
+                    <table className="table table-striped mt-3 custom-table">
+                        <thead>
+                            <tr>
+                                <th>ASIN</th>
+                                <th>Link</th>
+                                <th>Image</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rankingData.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.asin}</td>
+                                    <td>
+                                        <a href={item.link} target="_blank" rel="noopener noreferrer">Product Link</a>
+                                    </td>
+                                    <td>
+                                        <img src={item.image} alt={`Product ${index}`} style={{ width: "100px", height: "auto" }} />
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </>
+            );
         }
         return null;
-      };
-      
+    };
 
     return (
         <div className="container">
@@ -129,21 +122,22 @@ function MergedComponent() {
             </nav>
 
             <div className="content-section mt-3">
-                <h2 className="custom-h2">Scan the ASIN</h2>
+                <h2>Scan the ASIN</h2>
                 <input 
                     type="text" 
                     value={asin} 
                     onChange={handleChangeAsin} 
                     className="form-control mb-3" 
-                    placeholder="Enter ASIN" 
+                    placeholder="Scan ASIN" 
                 />
                 <button 
                     onClick={invokeLambdaFunction} 
-                    className="custom-button-color custom-button-center"
+                    className="btn btn-primary custom-button-center"
                 >
                     Submit
                 </button>
-                {renderResult()}
+                {isLoading && <img src={loadingGif} alt="Loading" className="loading-gif" />}
+                {!isLoading && renderResult()}
             </div>
         </div>
     );
